e8cd9075561d9be4d00b949780a7fe0dfa0ecd98
Maciej Smołka (mb) <smolka@agh.edu.pl>
2020-10-23 18:13:17 +0200

    Arg preparation capability in extsolver_simple

--- R/extsolver_simple.R
+++ R/extsolver_simple.R
@@ -10,6 +10,8 @@
 #' `NULL` indicates that the solver does not provide gradient info
 #' @param value_read_fn function reading the objective value from file
 #' @param gradient_read_fn function reading the objective gradient from file
+#' @param arg_combine_fn function producing appropriate length-1 character vector from
+#' point and precision for solver command line
 #'
 #' @return An object of classes `extsolver_simple` and `extsolver`
 #'
@@ -29,7 +31,8 @@ extsolver_simple <- function(
   value_file,
   gradient_file = NULL,
   value_read_fn = function(file) scan(file, quiet = TRUE),
-  gradient_read_fn = if (!is.null(gradient_file)) value_read_fn
+  gradient_read_fn = if (!is.null(gradient_file)) value_read_fn,
+  arg_combine_fn = function(x, precision) paste(c(x, precision), collapse = " ")
   ) {
   if (is.null(cmd)) {
     stop("Command to run solver must be provided", call. = FALSE)
@@ -43,7 +46,8 @@ extsolver_simple <- function(
       value_file = value_file,
       gradient_file = gradient_file,
       read_value = value_read_fn,
-      read_gradient = gradient_read_fn
+      read_gradient = gradient_read_fn,
+      combine_args = arg_combine_fn
     ),
     class = c("extsolver_simple", "extsolver")
   )
@@ -57,7 +61,7 @@ extsolver_simple <- function(
 compute_objective.extsolver_simple <- function(solver, x, precision, ...) {
   assert_point_not_null(x)
   assert_precision_not_null(precision)
-  cmd <- paste(solver$cmd, paste(x, collapse = " "), precision)
+  cmd <- paste(solver$cmd, solver$combine_args(x, precision))
   args <- list(...)
   if (is.null(args$ignore.stdout)) {
     args$ignore.stdout <- FALSE
